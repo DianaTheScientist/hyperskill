b97d6811e3e0a6a2799b680aa1c4589b95d15c1d
DianaTheScientist <152332099+DianaTheScientist@users.noreply.github.com>
2024-09-11 12:50:03 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Write your code here
+print('Earned amount:'
+      '\nBubblegum: $202'
+      '\nToffee: $118'
+      '\nIce cream: $2250'
+      '\nMilk chocolate: $1680'
+      '\nDoughnut: $1075'
+      '\nPancake: $80'
+      '\n\nIncome: $5405')
+
+print('Staff expenses:')
+s_expenses = int(input())
+print('Other expenses:')
+o_expenses = int(input())
+print(f'Net income: ${5405 - s_expenses - o_expenses}')
